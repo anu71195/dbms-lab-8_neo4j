91e0778d6bde8ace27a3bced4052a8ff9f800eec
anu <anu71195@gmail.com>
2018-04-13 10:31:28 +0530

    batch is working properly but slow php script written and working properly

--- insert2.py
+++ insert2.py
@@ -1,6 +1,6 @@
 import time,os,json,operator
 start_time=time.time();
-from py2neo import authenticate,Graph,Node,Relationship
+from py2neo import authenticate,Graph,Node,Relationship,Path
 authenticate('localhost:7474',"neo4j","pass")
 graph=Graph();
 tx=graph.begin();
@@ -60,6 +60,45 @@ for x in sorted(os.listdir('./workshop_dataset1')):
 	current_file_number+=1;
 	print("progress is = ",current_file_number,"/",113,"time=",time.time()-start_time)	
 	print(x)
-	for i in text:
-		print(text[i])
+	query="""
+	WITH {text} as data
+	unwind extract (key in keys(data) | data[key]) as twit
+	merge(ltid:tid{tid:twit.tid})ON CREATE
+		SET ltid.quote_count=twit.quote_count,ltid.like_count=twit.like_count,ltid.reply_count=twit.reply_count,ltid.verified=twit.verified,ltid.sentiment=twit.sentiments,ltid.retweet_count=twit.retweet_count,ltid.type=twit.type,ltid.tweet_text=twit.tweet_text
+	merge(llang:lang{lang:twit.lang})
+	merge(ltid)-[:has_lang]-(llang)
+	merge(lauthor:author_id{author_id:twit.author_id}) 
+	merge(lauthorn:author_name{author_name:twit.author})
+	merge(lauthors:authorsn{author_sn:twit.author_screen_name})
+	merge(lauthor)-[:has_sname]-(lauthors)
+	merge(lauthor)-[:has_name]-(lauthorn)
+	merge(ltid)-[:has_author_id]-(lauthor)
+
+	merge(ldate:dt{dtt:twit.datetime}) ON CREATE
+		SET ldate.date=twit.date
+	FOREACH (hashtagv in twit.hashtags|MERGE(lhashtag:hashtag{hashtag:hashtagv})
+	merge (ltid)-[:has_hashtag]-(lhashtag))
+
+	FOREACH (keywordv in twit.keywords_processed_list|MERGE(lkeyword:keyword{keyword:keywordv})
+	merge(ltid)-[:has_keyword]-(lkeyword))
+
+	FOREACH (mentionv in twit.mentions|MERGE(lmention:mention{mention:mentionv})
+	merge(ltid)-[:has_mention]-(lmention))
+
+	foreach(retweetv in twit.retweet_source_id|merge(lretweet:retweet_id{retweet_id:retweetv})
+	merge(ltid)-[:retweet]-(lretweet))
+
+	foreach(sourcev in twit.replyto_source_id|merge(lreply_source:reply_id{reply_id:sourcev})
+	merge(ltid)-[:reply_id]-(lreply_source)	)
+
+	foreach(locationv in twit.location|merge(llocation:location{location:locationv})
+	merge(ltid)-[:tweeted_from]-(llocation))
+
+	foreach(quotedv in twit.quoted_source_id|merge(lquoted:quoted_source{quoted_source:quotedv})
+	merge(ltid)-[:has_quotesi]-(lquoted))	
+	"""
+	graph.run(query,text=text)
+	
+
+
 print("total time=",time.time()-start_time);
